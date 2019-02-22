092a048d1260900a1f4a1e3f9ced6676315599c0
Bogdan <bogdan10050@gmail.com>
2019-02-22 15:26:13 +0200

    change clipPath text

--- src/components/ExpertiesCarousel/index.js
+++ src/components/ExpertiesCarousel/index.js
@@ -11,6 +11,7 @@ import {
  card_3,
  rigthArrow
 } from '@/images'
+import { throws } from 'assert';
 
 export default class ExpertiesCarousel extends React.PureComponent {
 
@@ -21,6 +22,10 @@ export default class ExpertiesCarousel extends React.PureComponent {
     this.state = {
       circlSize: 1,
     }
+  }
+
+  componentDidUpdate(){
+    console.log(this.state)
   }
 
   componentDidMount() {
@@ -87,25 +92,18 @@ export default class ExpertiesCarousel extends React.PureComponent {
     if (this.state.circlZooming) {
       if (this.state.circlSize === 1 && (e.deltaY < 0)) {
         this.setState({ circlZooming: false })
+        
       }
 
-      
-      const cs = this.state.circlSize + this.normalizeDelta(e)
+
+
+      let cs = this.state.circlSize * (this.normalizeDelta(e) / 5 + 1)
+      let mv = this.state.circlSize + this.normalizeDelta(e)
       this.setState({
-        circlSize: Math.min(23, Math.max(cs, 1)),
-        innerPageLock: cs >= 23
+        circlSize: Math.min(50, Math.max(cs, 1)),
+        innerPageLock: cs >= 50,
+        moveToLeft: Math.min(100, Math.max(mv, 1)) * 12
       })
-      
-      if(this.state.circlSize > 10){
-        this.setState({
-          step: 2.3
-        })
-      } else{
-        this.setState({
-          step: 1
-        })
-      }
-    
     }
 
     if (
@@ -114,7 +112,7 @@ export default class ExpertiesCarousel extends React.PureComponent {
       this.state.circlSize === 1 &&
       (e.deltaY > 0)
     ) {
-      this.setState({ circlZooming: true })    
+      this.setState({ circlZooming: true })
     }
   }
 
@@ -123,8 +121,6 @@ export default class ExpertiesCarousel extends React.PureComponent {
     const el = e.target.scrollingElement;    
     const verticalScrollLock = (this.scrollBottom(el) === 0)
 
-    
-
     if (!this.state.verticalScrollLock && verticalScrollLock) {
       this.html.classList.add('scroll-hidden')
       this.setState({ verticalScrollLock })
@@ -132,7 +128,7 @@ export default class ExpertiesCarousel extends React.PureComponent {
   }
 
   horizontalScrollHandler = (e) => {
-    if(this.clientWidth <= 768) return;
+    if(this.clientWidth <= 768) return
     const el = e.target;
     const circlZooming = (this.scrollLeft(el) === 0)
 
@@ -145,7 +141,7 @@ export default class ExpertiesCarousel extends React.PureComponent {
     return (
       <section className="scroll-section" ref={el => this.scrollSection = el}>
         <div className="cards">
-          <div className="card-home">
+          <div className='card-home' style={{ 'transform': 'translateX(' + -this.state.moveToLeft + 'vh' + ')' }}>
             <img src={card_1} />
             <div>
               <span>01</span>
@@ -154,7 +150,7 @@ export default class ExpertiesCarousel extends React.PureComponent {
               <a className="arrow" href="#" ><img src={rigthArrow} /></a>
             </div>
           </div>
-          <div className="card-home">
+          <div className='card-home' style={{ 'transform': 'translateX(' + -this.state.moveToLeft + 'vh' + ')' }}>
             <img src={card_2} />
             <div>
               <span>02</span>
@@ -163,7 +159,7 @@ export default class ExpertiesCarousel extends React.PureComponent {
               <a className="arrow" href="#" ><img src={rigthArrow} /></a>
             </div>
           </div>
-          <div className="card-home">
+          <div className='card-home' style={{ 'transform': 'translateX(' + -this.state.moveToLeft + 'vh' + ')' }}>
             <img src={card_3} />
             <div>
               <span>03</span>
@@ -179,7 +175,7 @@ export default class ExpertiesCarousel extends React.PureComponent {
           <span className="hide">{this.state.circlSize}</span>
           <svg x='0px' y='0px' viewBox='0 0 593.2 157.3'>
             <defs>
-            <clipPath id='text-borvo' style={{ 'transform' : `scale(${this.state.circlSize * this.state.step})` }} >
+            <clipPath id='text-borvo' style={{ 'transform' : `scale(${this.state.circlSize})` }} >
                 <path d='M0,154.5V0h35.7v55.8c8.8-11.1,20.8-16,33.1-16c28,0,49.3,22,49.3,58.6c0,38-21.8,58.8-49.3,58.8 c-12.5,0-23.9-5.1-33.1-15.8v13H0z M57,125.8c14.1,0,24.8-10.4,24.8-27.3c0-16.7-10.7-27.1-24.8-27.1c-7.6,0-16.9,4.2-21.3,10.2 v34.3C40.1,121.8,49.3,125.8,57,125.8z'/>
                 <path d='M129.7,98.4c0-31.5,22.7-58.6,60.7-58.6c38.5,0,61.2,27.1,61.2,58.6c0,31.5-22.7,58.8-61.2,58.8 C152.4,157.3,129.7,130,129.7,98.4z M215,98.4c0-14.8-8.8-27.1-24.6-27.1c-15.3,0-24.1,12.3-24.1,27.1c0,15.1,8.8,27.3,24.1,27.3 C206.2,125.8,215,113.5,215,98.4z'/>
                 <path d='M270.3,154.5V42.6H306v13.7c7.4-9,21.8-16.7,35-16.7v34.3c-2.1-0.7-5.1-0.9-8.8-0.9c-9.3,0-21.5,3.9-26.2,10.2 v71.3H270.3z'/>
