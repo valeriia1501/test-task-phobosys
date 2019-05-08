e5bea0c0c255a8f50d0f5b724d3012b3017a2d7f
Bogdan <bogdan10050@gmail.com>
2019-05-08 15:22:57 +0300

    add mobile burger-menu

--- src/store/RootStore.js
+++ src/store/RootStore.js
@@ -6,7 +6,8 @@ export class RootStore extends EventEmitter {
     constructor(args) {
         super(args)
         this._state = {
-            isOpen: false
+            isOpen: false,
+            isShowMobMenu: false
         }
         this.setMaxListeners(100)
     }
@@ -22,6 +23,11 @@ export class RootStore extends EventEmitter {
     togglePopUp = (isOpen = !this._state.isOpen) => {
         this._state.isOpen = isOpen
         this.emit(STATE_CHANGED, { ...this._state })
+    }
+
+    toggleMobMenu = (isShowMobMenu = !this._state.isShowMobMenu) => {
+        this._state.isShowMobMenu = isShowMobMenu
+        this.emit(STATE_CHANGED, {...this._state })
     }
 }
 
